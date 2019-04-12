37511ac845fdcf65a74d6148f62e11667c22282b
RubenD <rubendariopm14@gmail.com>
2019-04-11 21:32:00 -0400

    adding clamp and wrap functions

--- misc.h
+++ misc.h
@@ -19,4 +19,21 @@ fs::path concatPath(fs::path path, char * str);
 vector<fs::path> listFolder(fs::path _path);
 
 
+template<typename T>
+T clamp(T v, T l, T h) {
+  if (v < l)return l;
+  else if (v > h) return h;
+  else return v;
+}
+
+template<typename T>
+T wrap(T v, T l, T h) {
+  if (l > h)return v; else {
+    T r = h - l;
+    while (v < l)v += r;
+    while (v > h)v -= r;
+  }
+  return v;
+}
+
 #endif
